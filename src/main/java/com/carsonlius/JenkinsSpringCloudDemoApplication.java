fe213c2a7dd6b39d0609405f8a63f34bf2191efa
carson <carson_lius@163.com>
2023-11-21 17:17:35 +0800

    jenkins 部署springcloud项目

---
+++ src/main/java/com/carsonlius/JenkinsSpringCloudDemoApplication.java
@@ -0,0 +1,18 @@
+package com.carsonlius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version V1.0
+ * @author: carsonlius
+ * @date: 2023/11/21 17:10
+ * @company
+ * @description
+ */
+@SpringBootApplication
+public class JenkinsSpringCloudDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringCloudDemoApplication.class,args);
+    }
+}
